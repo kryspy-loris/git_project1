9b23daa09cc7e2b9cca952a4c076ada86a1d47a1
kryspy-loris <91410740+kryspy-loris@users.noreply.github.com>
2021-11-15 18:03:43 +0400

    Update main.py
    yan_primer

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(1234)
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+
+
+if __name__ == '__main__':
+    main()
